34965b0546dd4151a44aabe2fc2cdee72fc05b55
JoshuaOyed <joshua.oyedola@maxdrive.ai>
2023-11-17 10:51:43 +0100

    adding the sidebar UI

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,41 @@
 import Navbar from "../../components/navbar/Navbar";
 import Card from "../../components/card";
+import Sidebar from "../../components/sidebar/sidebar";
 
+// const Home = () => {
+//   return (
+//     <div className="flex">
+//       <div className="display flex">
+//       <Navbar />
+//       <Card/>
+//     </div>
+//     <div className="">
+//       <Sidebar/>
+//     </div>
+//     </div>
+    
+//   );
+// };
 const Home = () => {
   return (
-    <div>
-      <Navbar />
-      <Card/>
+    <div className="flex">
+     
+      <div className="flex-none">
+        <Sidebar />
+      </div>
+
+      <div className="flex flex-col flex-grow">
+        
+        <div className="bg-gray-50 text-white p-4">
+          <Navbar />
+        </div>
+        <div className="p-4">
+          <Card />
+        </div>
+      </div>
     </div>
   );
 };
+
 
 export default Home;
